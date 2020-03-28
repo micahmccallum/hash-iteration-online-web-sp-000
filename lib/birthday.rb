269e1c13a3360122dd66be74afdb006f7cf55cb7
Micah McCallum <micahmccallum@outlook.com>
2020-03-28 01:43:26 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,8 +10,8 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def happy_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!" if age < 12    
+    puts "Happy Birthday #{name}! You are now #{age} years old!" if age < 12
   end
 end
